80b0d605c19d56f6a362961e6de326cf4c401511
hyranie <hairani13@ukm.edu.my>
2023-09-05 09:49:32 +0800

    Update Urusetia.php

--- application/controllers/Urusetia.php
+++ application/controllers/Urusetia.php
@@ -13,7 +13,9 @@ class Urusetia extends CI_Controller {
 	public function dash_admin()
 	{
       //dbug($this->session->userdata("session_ukmper")); 
+      dbug('$_SESSION[]');
       dbug($_SESSION["session_ukmper"]);
+      dbug('$this->session->userdata()');
       dbug($this->session->userdata());
       die;
         $this->template->set('script_content', 'admin_f/script/script-dashboardlayout');
