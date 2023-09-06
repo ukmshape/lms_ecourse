4cf49fdf55c6ac4a29f3f25882aa2a681efb45d7
icaterall <ashqahman@gmail.com>
2023-09-06 10:32:30 +0800

    check if ci_session is writable

--- config.php
+++ config.php
@@ -386,7 +386,6 @@ $config['sess_save_path'] = APPPATH . 'ci_session/';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-
 /*
 |--------------------------------------------------------------------------
 | Cookie Related Variables
