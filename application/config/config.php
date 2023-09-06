cf50aea1367b85104b758a520ea8b160e4997222
icaterall <ashqahman@gmail.com>
2023-09-06 09:56:00 +0800

    update session driver

--- application/config/config.php
+++ application/config/config.php
@@ -383,7 +383,7 @@ $config['encryption_key'] = '987654321';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
+$config['sess_driver'] = 'redis';
 $config['sess_cookie_name'] = 'ci_sessions';
 $config['sess_expiration'] = 14400;
 $config['sess_save_path'] = APPPATH . 'ci_session/';
