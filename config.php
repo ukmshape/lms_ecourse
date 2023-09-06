7f90ca8c5f120dff86b85780d5c37d9c8891562a
icaterall <ashqahman@gmail.com>
2023-09-06 11:11:52 +0800

    check if ci_session is writable

--- config.php
+++ config.php
@@ -379,11 +379,10 @@ $config['encryption_key'] = '7rynne1JA3nfd9Wukm';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
-$config['sess_table_name'] = 'ci_sessions';
+$config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_sessions';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'ecourse';
+$config['sess_save_path'] = APPPATH . 'ci_session/'; // path to your ci_session folder
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
