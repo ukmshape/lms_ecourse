1490c7fcb8288c4806067aba0a72f0f413625098
icaterall <ashqahman@gmail.com>
2023-09-06 10:51:55 +0800

    check if ci_session is writable

--- application/config/config.php
+++ application/config/config.php
@@ -384,9 +384,9 @@ $config['encryption_key'] = '987654321';
 */
 $config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_sessions';
-$config['sess_expiration'] = 14400;
+$config['sess_expiration'] = 7200;
 $config['sess_save_path'] = 'ci_session';
-$config['sess_match_ip'] = TRUE;
+$config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 /*
